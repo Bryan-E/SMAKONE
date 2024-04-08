b5f230383d76464a0aacbb65e3211f1acc225640
Bryan-E <bryanedison27@gmail.com>
2024-04-08 13:23:10 +0700

    Update EmagzReader.js

--- smakone-website/src/Pages/Emagz/EmagzReader.js
+++ smakone-website/src/Pages/Emagz/EmagzReader.js
@@ -15,7 +15,7 @@ const EmagzReader = () => {
     return(
         <div>
             <Nav />           
-            <div className="container mx-auto justify h-screen w-screen pt-28" >
+            <div className="container mx-auto h-screen w-screen pt-28" >
                <HTMLFlipBook width={595} height={843} maxHeight={2160} maxWidth={3840} minHeight={100} minWidth={200} size="stretch" showCover={false} autoSize={false}>
                     {imageList.map((image, index) => (
                         <div className='page'><img src={image} alt={index} /></div>))}
